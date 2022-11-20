ba3dd22bd65d8fee2d4e37cf3d3471a1c8259292
voidrak <raklovetocode@gmail.com>
2022-11-20 10:31:06 +0300

    entered questions and answers for chemistry class

--- quiz app/main.cpp
+++ quiz app/main.cpp
@@ -43,6 +43,7 @@ int const total_question=5;
          std::cin>>subject_choice;
          switch (subject_choice)
          {
+            // BIOLOGY
          case 1:{
                      std::cout << " ****************INSTRUCTIONS!!***********\n";
          std::cout << "USE ONLY NUMBER  1-4 TO ANSWER THE QUESTIONS\n";
@@ -59,6 +60,7 @@ int const total_question=5;
          q5.askquestion(Score);
          std::cout << "you got " << Score << " out of " << total_question << std::endl;}
             break;
+            // MATHS
          case 2:{
             std::cout << " ****************INSTRUCTIONS!!***********\n";
          std::cout << "USE ONLY NUMBER  1-4 TO ANSWER THE QUESTIONS\n";
@@ -75,15 +77,16 @@ int const total_question=5;
          m5.askquestion(Score);
          std::cout << "you got " << Score << " out of " << total_question << std::endl;}
          break;
+         //CHEMISTRY
          case 3:{
              std::cout << " ****************INSTRUCTIONS!!***********\n";
          std::cout << "USE ONLY NUMBER  1-4 TO ANSWER THE QUESTIONS\n";
          std::cin.get();
-         ch1.setvalue("what is 2*3", "1.3", "2.6", "3.76", "4.9", 2);
-         ch2.setvalue("waht ", "1.Zooey Deschanel", "2.Kaley Cuoco", "3.Jennifer Aniston", "4.Alyson Hannigan ", 1);
-         ch3.setvalue("What is the largest canyon in the world", "1.Verdon Gorge, France", "2.King`s Canyon, Australia", "3.Grand Canyon, USA", "4.Fjaorargljufur Canyon, Iceland", 3);
-         ch4.setvalue("How long is the border between the United States and Canada", "1. 3,525 miles", "2. 4,525 miles", "3. 5,525 miles", "4. 6,525 miles", 2);
-         ch5.setvalue("What is the largest active volcano in the world", "1.Mount Etna", "2.Mount Vesuvius", "3.Mouna Loa", "4.Mount Batur", 4);
+         ch1.setvalue("The particle that has the smallest mass is the", "1. Electron", "2. Proton", "3. Neutron", "4. Nucleus", 1);
+         ch2.setvalue("The person given credit for developing the first modern periodic table is ", "1.Dalton", "2.Democritus", "3.Mendeleev", "4. Thomson ", 4);
+         ch3.setvalue("Two atoms are isotopes if they have", "1.Different atomic numbers", "2.The same mass number, but different atomic numbers", "3.The same number of protons and neutrons", "4.The same atomic number, but a different mass number", 4);
+         ch4.setvalue("The element found in the periodic table in Group 4 and Period 6 is", "1.Halfnium, Hf", "2.Selenium, Se", "3.Gadolinium, Gd", "4.Chromium, Cr", 3);
+         ch5.setvalue("A metaloid in Period 3 is", "1.Scandium", "2.Silicon", "3.Germanium", "4.Aluminum", 2);
          ch1.askquestion(Score);
          ch2.askquestion(Score);
          ch3.askquestion(Score);
